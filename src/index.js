ae34ec7e6536c81e368f9844878a3e6a1730c543
Yuttasak Pannawat <yuttasakcom@gmail.com>
2019-02-28 00:40:08 +0700

    create model dynamic relations

--- src/index.js
+++ src/index.js
@@ -18,6 +18,38 @@ const User = require('./models/user')
 
 const app = express()
 
+app.use(express.json())
+
+const events = eventIds => {
+  return Event.find({ _id: { $in: eventIds } })
+    .then(events => {
+      return events.map(event => {
+        return {
+          ...event._doc,
+          _id: event.id,
+          creator: user.bind(this, event.creator),
+        }
+      })
+    })
+    .catch(err => {
+      throw err
+    })
+}
+
+const user = userId => {
+  return User.findById(userId)
+    .then(user => {
+      return {
+        ...user._doc,
+        _id: user.id,
+        createdEvents: events.bind(this, user._doc.createdEvents),
+      }
+    })
+    .catch(err => {
+      throw err
+    })
+}
+
 const schema = buildSchema(`
   type Event {
     _id: ID!
@@ -25,13 +57,14 @@ const schema = buildSchema(`
     description: String!
     price: Float!
     date: String!
+    creator: User!
   }
 
   type User {
     _id: ID!
     email: String!
     password: String
-
+    createdEvents: [Event!]
   }
 
   input EventInput {
@@ -65,7 +98,13 @@ const root = {
   events: () => {
     return Event.find()
       .then(events => {
-        return events
+        return events.map(event => {
+          return {
+            ...event._doc,
+            _id: event.id,
+            creator: user.bind(this, event._doc.creator),
+          }
+        })
       })
       .catch(err => {
         console.log(err)
